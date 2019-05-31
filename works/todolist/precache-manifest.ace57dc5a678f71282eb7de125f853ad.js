eab8d807cfb0f5373555d8b8d1d9ca218410317f
chochi1st <chochi1st@gmail.com>
2019-05-31 19:37:17 +0800

    add

---
+++ works/todolist/precache-manifest.ace57dc5a678f71282eb7de125f853ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093be8a3798f03f5318da8146e3a636e",
+    "url": "/onlineshow/works/todolist/index.html"
+  },
+  {
+    "revision": "23b25fd1c17846d03432",
+    "url": "/onlineshow/works/todolist/static/css/main.8ad26d33.chunk.css"
+  },
+  {
+    "revision": "08e72e7f05dd5b0c7721",
+    "url": "/onlineshow/works/todolist/static/js/2.adf86020.chunk.js"
+  },
+  {
+    "revision": "23b25fd1c17846d03432",
+    "url": "/onlineshow/works/todolist/static/js/main.24af6bf2.chunk.js"
+  },
+  {
+    "revision": "65b6e5768a2fb4cb967b",
+    "url": "/onlineshow/works/todolist/static/js/runtime~main.05899764.js"
+  }
+]);
